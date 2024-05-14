a935fdda3ebe0f07baca16be5d2035fb064b3445
Mohammad Firoz Kabir <135244080+mdfirozkabir@users.noreply.github.com>
2024-05-14 19:13:05 +0600

    added chef-service section & complete

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({heading,subHeading}) => {
     return (
-        <div>
-            <p className='text-yellow-600 '>---{subHeading}---</p>
-            <h3>{heading}</h3>
+        <div className='my-8 mx-auto text-center w-4/12'>
+            <p className='text-yellow-600 mb-2 text-xl'>---{subHeading}---</p>
+            <h3 className='text-4xl uppercase border-y-4 py-4'>{heading}</h3>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
-
+import ChefService from '../chefService/ChefService';
+ 
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Category/>
+            <ChefService/>
         </div>
     );
 };
